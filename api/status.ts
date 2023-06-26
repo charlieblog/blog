2c79a1d8b6128f22dda17ba2254e8461671e6a4c
charliez0 <charliez0sp@gmail.com>
2023-06-26 16:25:40 +0800

    Site updated: 2023-06-26 16:25:40

---
+++ api/status.ts
@@ -0,0 +1,10 @@
+export default async function handler(req: Request) {
+    let header = new Headers();
+    header.set('Cache-Control', 's-maxage=63072000');
+
+    return new Response("OK", {
+        "status": 200,
+        "statusText": "OK",
+        "headers": header
+    });
+}
